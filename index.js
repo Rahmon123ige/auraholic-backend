7bc3f7d7cacb4051bdb80f1637d651d17a1f1971
Rahmon123ige <igerahmon@gmail.com>
2025-05-21 18:43:47 +0100

    Fix image upload URL

--- index.js
+++ index.js
@@ -44,7 +44,8 @@ app.post('/upload', upload.single('product'), (req, res) => {
 
   res.json({
     success: 1,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+    // image_url: `http://localhost:${port}/images/${req.file.filename}`,
+    image_url: `https://auraholic-backend.onrender.com/images/${req.file.filename}`,
   });
 });
 
